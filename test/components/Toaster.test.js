b564442d71de4996448cc4438adb4c5e5b24e10f
Jordi Fierro <jfierro@dexma.com>
2019-10-04 11:51:04 +0200

    test(toaster): test toaster component

--- test/components/Toaster.test.js
+++ test/components/Toaster.test.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { mount } from 'enzyme';
 
-import Toaster from 'components/Toaster';
+import Toaster, { ToasterContext } from 'components/Toaster';
 
 describe('<Toaster>', () => {
-  it('Should pass', () => {
-    expect(1).toEqual(1);
+  it('Should render a toast element when call toast', () => {
+    const toaster = mount(
+      <Toaster>
+        <ToasterContext.Consumer>
+          { context => context.toast('some alert') }
+        </ToasterContext.Consumer>
+      </Toaster>
+        );
+    expect(toaster.find('.toast').length).toEqual(1);
+  });
+  it('Should render nothing if toast not called', () => {
+    const toaster = mount(
+      <Toaster>
+        <ToasterContext.Consumer>
+          { context => {} }
+        </ToasterContext.Consumer>
+      </Toaster>
+    );
+    expect(toaster.find('.toast').length).toEqual(0);
+  });
+  it('Should render text passed', () => {
+    const toaster = mount(
+      <Toaster>
+        <ToasterContext.Consumer>
+          { context => context.toast('expected text') }
+        </ToasterContext.Consumer>
+      </Toaster>
+    );
+    expect(toaster.text()).toEqual('expected text');
   });
 });
